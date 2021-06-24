679969950b052dd1818af3f42885d3c0f8002ebc
shoaib <rahmanshoaibn@gmail.com>
2021-06-24 23:42:53 +0530

    delete button added

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -3,29 +3,31 @@ import { getMovies } from "../services/fakeMovieService";
 
 export default class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
   };
   render() {
     return (
       <table className="table">
         <thead>
           <tr>
-            <td>Title</td>
-            <td>Genre</td>
-            <td>Stock</td>
-            <td>Rate</td>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
-        { this.state.movies.map(movie => (
+          {this.state.movies.map((movie) => (
             <tr>
-            <td>{movie.title}</td>
-            <td>{movie.genre.name}</td>
-            <td>{movie.numberInStock}</td>
-            <td>{movie.dailyRentalRate}</td>
-          </tr>
-        ))}
-          
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td><button className="btn btn-danger btn-sm">Delete</button>
+            </td>
+              </tr>
+          ))}
         </tbody>
       </table>
     );
